c78988396e44b1d75e19bddaf5eaa2256899bd79
Brearey <brearey4@gmail.com>
2022-05-18 10:06:28 +0900

    # Added all digits

--- app/src/main/java/ru/oktemsec/yktwords/DigitalsActivity.kt
+++ app/src/main/java/ru/oktemsec/yktwords/DigitalsActivity.kt
@@ -11,7 +11,7 @@ import android.widget.Toast
 class DigitalsActivity : AppCompatActivity() {
 
     lateinit var mp: MediaPlayer
-    lateinit var mpDigitals: MediaPlayer
+    lateinit var mpVoice: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,34 +23,92 @@ class DigitalsActivity : AppCompatActivity() {
         mp.isLooping = true
         mp.start()
 
-        mpDigitals = MediaPlayer.create(this, R.raw.digitals)
-        mpDigitals.setVolume(0.15f, 0.15f)
-        if (!mpDigitals.isPlaying) { mpDigitals.start() }
-
-        // MediaPlayers of digits
-        val mpOne:MediaPlayer = MediaPlayer.create(this, R.raw.one)
-        val mpTwo:MediaPlayer = MediaPlayer.create(this, R.raw.two)
-        val mpThree:MediaPlayer = MediaPlayer.create(this, R.raw.three)
+        // Сыыппаралар
+        mpVoice = MediaPlayer.create(this, R.raw.digitals)
+        mpVoice.setVolume(0.15f, 0.15f)
+        if (!mpVoice.isPlaying) { mpVoice.start() }
 
         // Цифры кнопки
         val oneButton:ImageView = findViewById(R.id.one_image)
         val twoButton:ImageView = findViewById(R.id.two_image)
         val threeButton:ImageView = findViewById(R.id.three_image)
+        val fourButton:ImageView = findViewById(R.id.four_image)
+        val fiveButton:ImageView = findViewById(R.id.five_image)
+        val sixButton:ImageView = findViewById(R.id.six_image)
+        val sevenButton:ImageView = findViewById(R.id.seven_image)
+        val eightButton:ImageView = findViewById(R.id.eight_image)
+        val nineButton:ImageView = findViewById(R.id.nine_image)
+        val zeroButton:ImageView = findViewById(R.id.zero_image)
 
         //Нажатие кнопок
         oneButton.setOnClickListener {
             clickAnimation(it, 1.1f, 100)
-            mpOne.start()
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.one)
+            mpVoice.start()
         }
 
         twoButton.setOnClickListener {
             clickAnimation(it, 1.1f, 100)
-            mpTwo.start()
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.two)
+            mpVoice.start()
         }
 
         threeButton.setOnClickListener {
             clickAnimation(it, 1.1f, 100)
-            mpThree.start()
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.three)
+            mpVoice.start()
+        }
+
+        fourButton.setOnClickListener {
+            clickAnimation(it, 1.1f, 100)
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.four)
+            mpVoice.start()
+        }
+
+        fiveButton.setOnClickListener {
+            clickAnimation(it, 1.1f, 100)
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.five)
+            mpVoice.start()
+        }
+
+        sixButton.setOnClickListener {
+            clickAnimation(it, 1.1f, 100)
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.six)
+            mpVoice.start()
+        }
+
+        sevenButton.setOnClickListener {
+            clickAnimation(it, 1.1f, 100)
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.seven)
+            mpVoice.start()
+        }
+
+        eightButton.setOnClickListener {
+            clickAnimation(it, 1.1f, 100)
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.eight)
+            mpVoice.start()
+        }
+
+        nineButton.setOnClickListener {
+            clickAnimation(it, 1.1f, 100)
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.nine)
+            mpVoice.start()
+        }
+
+        zeroButton.setOnClickListener {
+            clickAnimation(it, 1.1f, 100)
+            mpVoice.reset()
+            mpVoice = MediaPlayer.create(this, R.raw.zero)
+            mpVoice.start()
         }
     }
 
@@ -94,6 +152,7 @@ class DigitalsActivity : AppCompatActivity() {
         }
     }
 
+    // Анимация кнопок при нажатии
     fun clickAnimation(v: View, value:Float, _duration:Long) {
         ObjectAnimator.ofFloat(v, "scaleX", value).apply {
             duration = _duration
